86d030cfbd9aab9e63a6e8a808e03b6d4db2a257
isuruferna <isurumadushan.ferna@gmail.com>
2025-01-09 18:00:52 +0100

    fix: font error fix

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Inter, Lustria } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lustria({weight: "400"})
+export const lusitana = Lustria({subsets: ['latin'], weight: "400"})
