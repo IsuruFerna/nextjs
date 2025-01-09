1db752c1b6ee2982c404da2c125a7e317675ae2f
isuruferna <isurumadushan.ferna@gmail.com>
2025-01-09 17:54:56 +0100

    my first next.js app

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>This is Customer page</p>
+  )
+}
+
+export default page
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <p>This is Invoice page</p>
+  )
+}
+
+export default page
